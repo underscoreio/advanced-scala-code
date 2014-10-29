ecc8b19f835532739def905fd627116a744ad2b2
Noel Welsh <noel@untyped.com>
2014-10-29 05:29:24 -0700

    Exampe of caller error recovery

--- monad/src/main/scala/ErrorHandling.scala
+++ monad/src/main/scala/ErrorHandling.scala
@@ -1,6 +1,6 @@
 import scala.language.higherKinds
 
-import scalaz.{Id, Monoid, MonadPlus, \/}
+import scalaz.{Id, Monoid, Monad, MonadPlus, \/}
 import scalaz.Id._
 import scalaz.std.option._
 import scalaz.syntax.monoid._
@@ -13,7 +13,7 @@ object MonadPlusExamples {
   none[Int] <+> some(3)
 }
 
-object FoldMap {
+object RecoveringFoldMap {
   def foldMapM[A, M[_] : MonadPlus, B: Monoid](iter: Iterable[A])(f: A => M[B] = (a: A) => a.point[Id]): M[B] =
     iter.foldLeft(mzero[B].point[M]){ (accum, elt) =>
       for {
@@ -24,12 +24,12 @@ object FoldMap {
 
   implicit class IterableFoldMappable[A](iter: Iterable[A]) {
     def foldMapM[M[_] : MonadPlus, B : Monoid](f: A => M[B] = (a: A) => a.point[Id]): M[B] =
-      FoldMap.foldMapM(iter)(f)
+      RecoveringFoldMap.foldMapM(iter)(f)
   }
 }
 
-object Examples {
-  import FoldMap._
+object RecoveringExamples {
+  import RecoveringFoldMap._
   import scala.concurrent.ExecutionContext.Implicits.global
 
   def examples = {
@@ -37,5 +37,33 @@ object Examples {
     import scalaz.std.option._
 
     Seq(1, 2, 3).foldMapM(a => if(a % 2 == 0) some(a) else none[Int])
+  }
+}
+
+object RecoveringToolkit {
+  def foldMapM[A, M[_] : Monad, B: Monoid](iter: Iterable[A])(f: A => M[B] = (a: A) => a.point[Id]): M[B] =
+    iter.foldLeft(mzero[B].point[M]){ (accum, elt) =>
+      for {
+        a <- accum
+        b <- f(elt)
+      } yield a |+| b
+    }
+
+  implicit class IterableFoldMappable[A](iter: Iterable[A]) {
+    def foldMapM[M[_] : Monad, B : Monoid](f: A => M[B] = (a: A) => a.point[Id]): M[B] =
+      RecoveringToolkit.foldMapM(iter)(f)
+  }
+
+  def recover[A, M[_] : MonadPlus, B : Monoid](f: A => M[B]): (A => M[B]) = {
+    val identity = mzero[B].point[M]
+    a => (f(a) <+> identity)
+  }
+
+  def example = {
+    import scalaz.std.anyVal._
+    import scalaz.std.option._
+    import scalaz.syntax.std.string._
+
+    Seq("1", "b", "3").foldMapM(recover(_.parseInt.toOption))
   }
 }
